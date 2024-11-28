3c807c04c69d73531d4808bc52c18ea56a1c555d
AssaadWansa <assaadwansa123@gmail.com>
2024-11-28 16:15:53 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sudoku from './Sudoku';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>NavyBits Sudoku</h1>
+      </header>
+      <Sudoku />
+    </div>
+  );
+}
+
+export default App;
