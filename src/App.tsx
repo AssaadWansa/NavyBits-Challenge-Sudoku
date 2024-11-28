8a661181d588cd257a1084d2ea02934f59ee0f3d
AssaadWansa <assaadwansa123@gmail.com>
2024-11-29 00:16:45 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 import Sudoku from './Components/Sudoku';
 import './Styles/App.css';
-=======
-import React from 'react';
-import Sudoku from './Sudoku';
-import './App.css';
->>>>>>> 03e09f74f4b7ffcb131d53e0210ee6844e9c7417
 
 function App() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import './Styles/index.css';
 import App from './App';
-=======
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
->>>>>>> 03e09f74f4b7ffcb131d53e0210ee6844e9c7417
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,11 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-=======
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> 03e09f74f4b7ffcb131d53e0210ee6844e9c7417
